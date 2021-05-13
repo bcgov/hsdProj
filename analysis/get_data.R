0287c470499a2b1d0a056c6b2d96833451074a2c
JulieHawkins <Julie.Hawkins@gov.bc.ca>
2021-05-13 09:52:28 -0700

    minor edits, replaced exists() with file.exists(), ensure that BC is always ID 0, now also save timestamped RDS to archive

--- analysis/get_data.R
+++ analysis/get_data.R
@@ -18,7 +18,7 @@ data.pretty <- function(base_folder, file_name, file_type, mysheet, data_cols, f
   if(file_type == "csv"){
     
     ## read in csv data
-    df <- readr::read_csv(file = paste0(base_folder, file_name, ".csv"))   #,col_names = TRUE, col_specs)
+    df <- readr::read_csv(file = paste0(base_folder, file_name, ".csv"))
     
   } else {
     
@@ -38,7 +38,7 @@ data.pretty <- function(base_folder, file_name, file_type, mysheet, data_cols, f
   df <- df %>% select(Region, Region.Type, Year, Total)
   
   ## if analysis/inputs/lookup.csv does not exist, make it (no longer working b/c data changed)
-  if(!exists(here("analysis", "inputs", "lookup.csv"))){
+  if(!file.exists(here("analysis", "inputs", "lookup.csv"))){
     ## requires: "analysis/inputs/REGNAMES_from_Access.csv"
     ## made manually by opening Database work/WorkingFile.accdb and copying REGNAMES into Excel
     source(here("analysis", "make_lookup.R"))
@@ -47,30 +47,48 @@ data.pretty <- function(base_folder, file_name, file_type, mysheet, data_cols, f
   ## open lookup and join in Region.Names
   lookup <- readr::read_csv(here("analysis", "inputs", "lookup.csv"),
                             col_names = TRUE, col_types = "dcc")
+  
   df <- left_join(df, lookup, by = c("Region", "Region.Type")) %>%
     select(Region, Region.Name = NAME, everything())
+
+  ## ensure that British Columbia is always Region 0
+  ### get any Region.Types with a non-0 BC
+  changeBCregion <- df %>% 
+    filter(Region.Name == "British Columbia", Region != 0) %>% 
+    select(Region.Type) %>% unique() %>% pull()
+  
+  # regions <- df %>% select(Region.Type) %>% unique() %>% pull()
+  for(i in seq_along(changeBCregion)) {
+    if(all(df %>% filter(Region.Name == "British Columbia", Region.Type %in% changeBCregion[i]) %>% 
+      select(Region) %>% unique() %>% pull() != 0)) {
+      df <- df %>% mutate(Region = case_when(Region.Name == "British Columbia" & Region.Type %in% changeBCregion[i] ~ 0,
+                                             TRUE ~ Region))
+    }
+  }
 
   ## replace Region.Type abbreviations with names
   df <- df %>% mutate(Region.Type = case_when(
     Region.Type == "CF" ~ "Children and Family Development",
     Region.Type == "DR" ~ "Development Region",
     Region.Type == "HA" ~ "Local Health Area",
-    Region.Type == "HY" ~ "Health Authority",
+    Region.Type == "HY" ~ "Health Authority",  ## note that HY 6 is BC, there may be no 0 in data
     Region.Type == "HS" ~ "Health Service Delivery Area",
     Region.Type == "PS" ~ "College Region",
     Region.Type == "RD" ~ "Regional District",
     Region.Type == "SD" ~ "School District",
     Region.Type == "SR" ~ "Special Regions (CMAs and Vancouver Island)",
-	  Region.Type == "CH" ~ "Community Health Service Area",
-    TRUE ~ as.character(Region.Type)
-    )
+    Region.Type == "CH" ~ "Community Health Service Area",
+    TRUE ~ as.character(Region.Type))
   )
-
+  
   df <- df %>% filter(!is.na(Region.Name))
 
   ## save as RDS
   saveRDS(df, paste0("app/data/data", ".rds"))
-
+  
+  ## save again in Archive folder with date
+  saveRDS(df, paste0("app/data/Archive/data", age_var, "_", Sys.Date(), ".rds"))
+  
   ## return df
   df
 }
